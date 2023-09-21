ffbb47257fc3b160603a4b466d739edad0b0253c
Jae Yong Lee <leejy518@gmail.com>
2023-09-21 10:20:46 -0500

    Replaced Card function to Row function

--- app/src/main/java/com/icode/co_opt_1/MainActivity.kt
+++ app/src/main/java/com/icode/co_opt_1/MainActivity.kt
@@ -3,14 +3,26 @@ package com.icode.co_opt_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.icode.co_opt_1.ui.theme.Co_opt_1Theme
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +34,60 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    JetpackCompose()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//@Composable
+//fun JetpackCompose() {
+//    Card {
+//        var expanded by remember { mutableStateOf(false) }
+//        Column(Modifier.clickable { expanded =!expanded }) {
+//            Image(painterResource(R.drawable.test), "Card image")
+//            AnimatedVisibility(expanded) {
+//                Text(
+//                    text = "Animal",
+//                    style = MaterialTheme.typography.bodyLarge
+//                )
+//            }
+//        }
+//    }
+//}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Co_opt_1Theme {
-        Greeting("Android")
+fun JetpackCompose() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+
+        Column() {
+            var expanded by remember { mutableStateOf(false) }
+            Column(Modifier.clickable { expanded =!expanded }) {
+                Image(painterResource(R.drawable.test), "Card image")
+                AnimatedVisibility(expanded) {
+                    Text(
+                        text = "Animal",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+        }
+
+        Column() {
+            var expanded by remember { mutableStateOf(false) }
+            Column(Modifier.clickable { expanded =!expanded }) {
+                Image(painterResource(R.drawable.test), "Card image")
+                AnimatedVisibility(expanded) {
+                    Text(
+                        text = "Animal",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+        }
+
     }
 }
