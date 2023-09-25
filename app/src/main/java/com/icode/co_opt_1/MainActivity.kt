0f02e30f27b9258582a5722946df9a02d63d5a4f
IMJ518 <leejy518@gmail.com>
2023-09-24 23:36:42 -0500

    Added horizontalScroll modifier

--- app/src/main/java/com/icode/co_opt_1/MainActivity.kt
+++ app/src/main/java/com/icode/co_opt_1/MainActivity.kt
@@ -6,10 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,7 +27,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.icode.co_opt_1.ui.theme.Co_opt_1Theme
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,53 +53,109 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//@Composable
-//fun JetpackCompose() {
-//    Card {
-//        var expanded by remember { mutableStateOf(false) }
-//        Column(Modifier.clickable { expanded =!expanded }) {
-//            Image(painterResource(R.drawable.test), "Card image")
-//            AnimatedVisibility(expanded) {
-//                Text(
-//                    text = "Animal",
-//                    style = MaterialTheme.typography.bodyLarge
-//                )
-//            }
-//        }
-//    }
-//}
-
 @Composable
 fun JetpackCompose() {
     Row(
-        horizontalArrangement = Arrangement.SpaceEvenly
+        Modifier.horizontalScroll(rememberScrollState()),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+
+
     ) {
-
-        Column() {
+        Column(Modifier
+            .padding(20.dp)
+            .fillMaxWidth()) {
             var expanded by remember { mutableStateOf(false) }
-            Column(Modifier.clickable { expanded =!expanded }) {
-                Image(painterResource(R.drawable.test), "Card image")
-                AnimatedVisibility(expanded) {
-                    Text(
-                        text = "Animal",
-                        style = MaterialTheme.typography.bodyLarge
+
+            Text(
+                text = "First animal",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Image(painterResource(R.drawable.test), "Card image",
+                Modifier
+                    .requiredSize(350.dp)
+                    .align(CenterHorizontally)
+                    .clickable { expanded = !expanded })
+            AnimatedVisibility(expanded) {
+                Text(
+                    text = "Animal",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
                     )
-                }
+                )
             }
         }
 
-        Column() {
+        Column(Modifier
+            .padding(20.dp)
+            .fillMaxWidth()) {
             var expanded by remember { mutableStateOf(false) }
-            Column(Modifier.clickable { expanded =!expanded }) {
-                Image(painterResource(R.drawable.test), "Card image")
-                AnimatedVisibility(expanded) {
-                    Text(
-                        text = "Animal",
-                        style = MaterialTheme.typography.bodyLarge
+
+            Text(
+                text = "Second animal",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Image(painterResource(R.drawable.test), "Card image",
+                Modifier
+                    .requiredSize(350.dp)
+                    .align(CenterHorizontally)
+                    .clickable { expanded = !expanded })
+            AnimatedVisibility(expanded) {
+                Text(
+                    text = "Animal",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
                     )
-                }
+                )
             }
         }
 
+        Column(Modifier
+            .padding(20.dp)
+            .fillMaxWidth()) {
+            var expanded by remember { mutableStateOf(false) }
+
+            Text(
+                text = "Third animal",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Image(painterResource(R.drawable.test), "Card image",
+                Modifier
+                    .requiredSize(350.dp)
+                    .align(CenterHorizontally)
+                    .clickable { expanded = !expanded })
+            AnimatedVisibility(expanded) {
+                Text(
+                    text = "Animal",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+        }
     }
 }
